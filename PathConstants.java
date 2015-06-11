f1bfcf66461d9552f48442604200d29556751ff2
manishajain28 <manisha_jain_28@yahoo.co.in>
2015-06-12 02:48:20 +0530

    Create PathConstants.java
    defining the path constants.

---
+++ PathConstants.java
@@ -0,0 +1,12 @@
+package com.manisha.assignment;
+
+
+public class PathConstants {
+
+	public static final String INVALID_GATE = "InvalidGate";
+	
+	public static final String PATH_NOT_FOUND = "PathNotFound";
+	
+	public static final String NULL_POINTER_EXCEPTION = "NullPointerException";
+}
+
